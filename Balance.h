e569ec0587760d42d873d88c068f6ef17af75ee9
CommunistMountain <greencrosshair@gmail.com>
2025-12-09 10:01:05 +0800

    Initial commit for scaleDRA

--- Balance.h
+++ Balance.h
@@ -1,5 +1,6 @@
 extern bool g_hackingDroneFix;
 extern bool g_repairDroneRecoveryFix;
+extern bool g_scaleDRA;
 extern bool g_controllableIonDroneFix;
 extern float g_controllableIonDroneFix_Delay;
 extern float g_controllableIonDroneFix_DelayInitial;
